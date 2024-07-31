9312efe652d3ade06e4fef0ce144ed8e69c7fef0
lightningcloud-git <ujjisamhita@gmail.com>
2024-07-31 16:34:41 -0500

    Update memory.js

--- memory.js
+++ memory.js
@@ -2,42 +2,63 @@ let level = 0;
 
 function MemoryGame() {
 
-level = 5;
-
-
-nextSequence();
+level = 0;
+start();
 
 
 }
 
-function nextSequence() {
-    level++;
-    
-for (let i = 0; i < level; i++) {
-var randomNumber = Math.floor(Math.random() * 4);
-    console.log(randomNumber)
-    randomNumber = randomNumber + 1;
-    console.log(randomNumber)
-    
-    setTimeout(function() {
-  	flash(randomNumber);
+ async function start() {
 
-}, 500);
-    
-    }
-    
-    
+await nextSequence();
+var matchValue = match();
+if (matchValue)
+  { 
+     if(level < 5) {
+     await start();
+     }
+     
+  }
+
 }
 
-function flash(randomNumber){
-
+ function flash(randomNumber) {
+      return new Promise((resolve, reject) => {
+          console.log("flash");
 var block = "block"+randomNumber;
 document.getElementById(block).style.backgroundColor = "Green";
 
 setTimeout(function() {
   	document.getElementById(block).style.backgroundColor = "Grey";
+    resolve(randomNumber);
+
 
 }, 500);
+      })
+    }
+
+
+async function nextSequence() {
+    
+    level++;
+    console.log("level "+level)
+		for (let i = 0; i < level; i++) {
+    var randomNumber = Math.floor(Math.random() * 4);
+    randomNumber = randomNumber + 1;
+    console.log("block "+randomNumber);
+    const result = await flash(randomNumber)
+    console.log(result)
+    
+    }
+        
+       
+    }
+
+
+
+function match(){
+
+return true;
 
 }
 
